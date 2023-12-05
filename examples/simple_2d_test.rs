8df1ed5686a0c033ebc63fd1699f76d4577196d9
a.yamaev <a.yamaev@smartengines.com>
2023-12-05 13:31:51 +0300

    Test 2D and 3D

---
+++ examples/simple_2d_test.rs
@@ -0,0 +1,90 @@
+use bevy::prelude::*;
+use rand::prelude::*;
+
+const TREE_PATH: &str = "test/tree.png";
+const SHEEP_PATH: &str = "test/sheep.png";
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands : Commands,
+    asset_server: Res<AssetServer>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    //spawn big grass texture
+
+    commands.spawn(
+        SpriteBundle {
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, -1000.0)),
+            sprite : Sprite {
+                custom_size: Some(Vec2::new(1000000.0, 1000000.0)),
+                color: Color::rgb(0.0, 0.5, 0.0),
+                ..default()
+            },
+            ..default()
+        }
+    );
+
+    let r = 300.0;
+
+    let sheep_count = 2000;
+
+    let mut rng = rand::thread_rng();
+    for _ in 0..sheep_count {
+        let random_pos = Vec2::new(
+            rng.gen_range(-r..r),
+            rng.gen_range(-r..r),
+        );
+
+        if random_pos.length() > r {
+            continue;
+        }
+
+
+        commands
+            .spawn(SpriteBundle {
+                texture: asset_server.load(SHEEP_PATH),
+                transform: Transform::from_translation(random_pos.extend(-random_pos.y)),
+                sprite : Sprite {
+                    custom_size: Some(Vec2::new(60.0, 60.0)),
+                    ..default()
+                },
+                ..default()
+            });
+    }
+
+    //spawn trees
+
+    let tree_count = 3000;
+    let tree_r = 1000.0;
+    let cut_r = r + 200.0;
+
+    for _ in 0..tree_count {
+        let random_pos = Vec2::new(
+            rng.gen_range(-tree_r..tree_r),
+            rng.gen_range(-tree_r..tree_r),
+        );
+
+        if random_pos.length() < cut_r {
+            continue;
+        }
+
+        commands
+            .spawn(SpriteBundle {
+                texture: asset_server.load(TREE_PATH),
+                transform: Transform::from_translation(random_pos.extend(-random_pos.y)),
+                sprite : Sprite {
+                    custom_size: Some(Vec2::new(100.0, 100.0)),
+                    ..default()
+                },
+                ..default()
+            });
+    }
+}
---
+++ examples/simple_3d_test.rs
@@ -0,0 +1,120 @@
+use std::f32::consts::PI;
+
+use bevy::prelude::*;
+use rand::prelude::*;
+
+const TREE_PATH: &str = "test/pine.png";
+const SHEEP_PATH: &str = "test/sheep.png";
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>) {
+
+    commands.spawn(Camera3dBundle {
+        transform : Transform::from_xyz(0.0, 0.0, -100.0).looking_at(Vec3::ZERO, Vec3::Y).with_translation(Vec3::new(0.0, -10.0, -150.0)),
+        ..default()
+    });
+
+    //green plane
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 1000.0, ..default() })),
+        material: materials.add(StandardMaterial {
+            base_color: Color::rgb(0.0, 0.5, 0.0),
+            ..default()
+        }),
+        transform: Transform::from_xyz(0.0, 0.0, 0.0).with_rotation(Quat::from_euler(EulerRot::XYZ, -PI / 4.0, 0.0, 0.0)),
+        ..default()
+    });
+
+    //spawn sun
+    commands.spawn(DirectionalLightBundle {
+        transform: Transform::from_xyz(10.0, 10.0, -10.0).looking_at(Vec3::ZERO, Vec3::Y),
+        directional_light: DirectionalLight {
+            shadows_enabled: true,
+            color: Color::WHITE,
+            illuminance: 50000.0,
+            ..default()
+        },
+        ..default()
+    });
+
+    //ambient ligjt
+    commands.insert_resource(AmbientLight {
+        color: Color::WHITE,
+        brightness: 1.0,
+    });
+
+    let square = meshes.add(shape::Plane { size: 1.0, ..default() }.into());
+    let sheep_texture : Handle<Image> = asset_server.load(SHEEP_PATH);
+    let tree_texture: Handle<Image> = asset_server.load(TREE_PATH);
+
+    let sheep_material = materials.add(StandardMaterial {
+        base_color_texture: Some(sheep_texture),
+        alpha_mode: AlphaMode::Blend,
+        ..default()
+    });
+
+    //spawn ships
+    let r = 50.0;
+    let mut rng = rand::thread_rng();
+    let sheep_count = 2000;
+
+    for _ in 0..sheep_count {
+        let x = rng.gen_range(-r..r);
+        let y = rng.gen_range(-r..r);
+        let z = y;
+
+        let pos = Vec3::new(x, y, z);
+        if pos.length() > r {
+            continue;
+        }
+
+        commands.spawn(PbrBundle {
+            mesh: square.clone(),
+            material: sheep_material.clone(),
+            transform: Transform::from_xyz(pos.x, pos.y + 4.0, pos.z).with_rotation(Quat::from_euler(EulerRot::XYZ, -PI / 2.0, PI, 0.0)).with_scale(Vec3::splat(10.0)),
+            ..default()
+        });
+    }
+
+
+    //spawn trees
+    let tree_count = 5000;
+    let tree_material = materials.add(StandardMaterial {
+        base_color_texture: Some(tree_texture),
+        alpha_mode: AlphaMode::Blend,
+        perceptual_roughness: 0.1,
+        ..default()
+    });
+
+    let tree_r = 150.0;
+    let cut_r = r + 20.0;
+
+    for _ in 0..tree_count {
+        let x = rng.gen_range(-tree_r..tree_r);
+        let y = rng.gen_range(-tree_r..tree_r);
+        let z = y;
+
+        let pos = Vec3::new(x, y, z);
+        if pos.length() < cut_r {
+            continue;
+        }
+
+        commands.spawn(PbrBundle {
+            mesh: square.clone(),
+            material: tree_material.clone(),
+            transform: Transform::from_xyz(pos.x, pos.y + 8.0, pos.z).with_rotation(Quat::from_euler(EulerRot::XYZ, -PI / 2.0, PI, 0.0)).with_scale(Vec3::new(10.0, 10.0, 20.0)),
+            ..default()
+        });
+    }
+}
