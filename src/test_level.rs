9041deacf22e63497c5844f5b8d8fd85f2acf848
a.yamaev <a.yamaev@smartengines.com>
2023-12-06 15:23:21 +0300

    Add hdr camera

--- src/test_level.rs
+++ src/test_level.rs
@@ -16,6 +16,10 @@ pub fn setup(
 ) {
     commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(0.0, 100.0, 100.0).looking_at(Vec3::ZERO, Vec3::Y),
+        camera : Camera {
+            hdr: true,
+            ..default()
+        },
         ..default()
     });
 
